2f626805c16f506cb91fbf43403e85bb54fdeb5b
HONG DOU ZHANG <1914144764@qq.com>
2016-10-19 15:08:50 +0800

    update to swift3.0

--- 图片轮播/CycleScrollView.swift
+++ 图片轮播/CycleScrollView.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l < r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l > r
+  default:
+    return rhs < lhs
+  }
+}
+
 
 /**
  数据源：获取总的page个数
  **/
-typealias TotalPagesCountBlock  = (totalPgaesCount: NSInteger) -> Void
+typealias TotalPagesCountBlock  = (_ totalPgaesCount: NSInteger) -> Void
 /**
  数据源：获取第pageIndex个位置的contentView
  **/
-typealias ContentViewAtIndex = (pageIndex: NSInteger) -> UIView
+typealias ContentViewAtIndex = (_ pageIndex: NSInteger) -> UIView
 /**
  当点击的时候，执行的block
  **/
-typealias ActionBlock = (pageIndex: NSInteger) -> Void
+typealias ActionBlock = (_ pageIndex: NSInteger) -> Void
 class CycleScrollView: UIView, UIScrollViewDelegate {
     //存放加载图片的网址
     var imageURLArray = Array<String>()
@@ -27,14 +47,14 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
     
     var scrollView : UIScrollView?
 //    var getTotalPagesCountBlock = TotalPagesCountBlock?()
-    var fetchContentViewAtIndex = ContentViewAtIndex?()
-    var tapActionBlock = ActionBlock?()
+    var fetchContentViewAtIndex : ContentViewAtIndex?
+    var tapActionBlock : ActionBlock?
     var pageControl : UIPageControl?
     var currentPageIndex = NSInteger()
     
     var contentViews = NSMutableArray()
-    var animationTimer : NSTimer?
-    var animationDuration = NSTimeInterval()
+    var animationTimer : Timer?
+    var animationDuration = TimeInterval()
     
     var _totalPageCount : NSInteger?
     var totalPageCount : NSInteger? {
@@ -43,10 +63,10 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
             if _totalPageCount > 0 {
                 
                 if _totalPageCount > 1 {
-                    self.scrollView?.scrollEnabled = true
+                    self.scrollView?.isScrollEnabled = true
                     self.startTimer()
                     if _totalPageCount == 2 {
-                        self.imageViewCopy = UIImageView.init(frame: CGRectMake(0, 0, CGRectGetWidth(self.frame), 200))
+                        self.imageViewCopy = UIImageView.init(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: 200))
                     }
                 }
                 self.configContentViews()
@@ -60,21 +80,21 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
     /**
      如果直接就是init的话，就需要加override
      */
-    init(frame: CGRect, animationDuration: NSTimeInterval) {
+    init(frame: CGRect, animationDuration: TimeInterval) {
         super.init(frame: frame)
         self.animationDuration = animationDuration
         self.autoresizesSubviews = true
-        let scrollViewFrame = CGRectMake(0, 0, CGRectGetWidth(UIScreen.mainScreen().bounds), frame.size.height)
+        let scrollViewFrame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: frame.size.height)
         self.scrollView = UIScrollView.init(frame: scrollViewFrame)
         self.scrollView?.showsVerticalScrollIndicator = false
         self.scrollView?.showsHorizontalScrollIndicator = false
-        self.scrollView?.contentMode = .Center
-        self.scrollView?.scrollEnabled = false
-        self.scrollView?.contentSize = CGSizeMake(3 * CGRectGetWidth(scrollViewFrame), CGRectGetHeight(scrollViewFrame))
+        self.scrollView?.contentMode = .center
+        self.scrollView?.isScrollEnabled = false
+        self.scrollView?.contentSize = CGSize(width: 3 * scrollViewFrame.width, height: scrollViewFrame.height)
         self.scrollView?.delegate = self
-        self.scrollView?.pagingEnabled = true
+        self.scrollView?.isPagingEnabled = true
         self.addSubview(self.scrollView!)
-        self.pageControl = UIPageControl.init(frame: CGRectMake((scrollViewFrame.size.width/2 - 20), (scrollViewFrame.size.height - 20), 40, 30))
+        self.pageControl = UIPageControl.init(frame: CGRect(x: (scrollViewFrame.size.width/2 - 20), y: (scrollViewFrame.size.height - 20), width: 40, height: 30))
         self.pageControl?.currentPage = 0
         self.addSubview(self.pageControl!)
         self.currentPageIndex = 0
@@ -84,7 +104,7 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
             self.animationTimer?.invalidate()
             self.animationTimer = nil
         }
-        self.animationTimer = NSTimer.scheduledTimerWithTimeInterval(animationDuration, target: self, selector: #selector(CycleScrollView.animationTimerDidFired), userInfo:nil, repeats: true)
+        self.animationTimer = Timer.scheduledTimer(timeInterval: animationDuration, target: self, selector: #selector(CycleScrollView.animationTimerDidFired), userInfo:nil, repeats: true)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -99,21 +119,21 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
         var counter : CGFloat = 0
         for contentView in self.contentViews {
             let view = contentView as! UIView
-            view.userInteractionEnabled = true
+            view.isUserInteractionEnabled = true
             let tap = UITapGestureRecognizer.init(target: self, action: #selector(CycleScrollView.contentViewTapAction(_:)))
             view.addGestureRecognizer(tap)
             var rightRect = view.frame
             
-            rightRect.origin = CGPointMake(CGRectGetWidth(UIScreen.mainScreen().bounds) * counter, 0)
+            rightRect.origin = CGPoint(x: UIScreen.main.bounds.width * counter, y: 0)
             counter += 1
             view.frame = rightRect
             self.scrollView?.addSubview(view)
         }
         
         if _totalPageCount == 1 {
-            self.scrollView?.contentOffset = CGPointMake(0, 0)
+            self.scrollView?.contentOffset = CGPoint(x: 0, y: 0)
         }else{
-            self.scrollView?.contentOffset = CGPointMake(self.scrollView!.frame.size.width, 0)
+            self.scrollView?.contentOffset = CGPoint(x: self.scrollView!.frame.size.width, y: 0)
         }
     }
     func setScrollViewContentDataSource() {
@@ -122,32 +142,36 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
         self.contentViews.removeAllObjects()
         if self.fetchContentViewAtIndex != nil {
             if _totalPageCount == 1 {
-                self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: 0))
+                self.contentViews.add(self.fetchContentViewAtIndex!(0))
             }else{
-                if _totalPageCount == 2 {
+                if _totalPageCount == 2
+                
+                
+                {
                     //如果当前是0的话，就copy1
                     if self.currentPageIndex == 0 {
-                        self.imageViewCopy?.sd_setImageWithURL(NSURL(string: self.imageURLArray[1]))
-                        self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: previousPageIndex))
-                        self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: self.currentPageIndex))
-                        self.contentViews.addObject(self.imageViewCopy!)
+                        self.imageViewCopy?.sd_setImage(with: URL(string: self.imageURLArray[1]))
+                        self.contentViews.add(self.fetchContentViewAtIndex!(previousPageIndex))
+                        self.contentViews.add(self.fetchContentViewAtIndex!(self.currentPageIndex))
+                        self.contentViews.add(self.imageViewCopy!)
                         
                     }else{//如果当前是1的话，就copy0
-                        self.imageViewCopy?.sd_setImageWithURL(NSURL(string: self.imageURLArray[0]))
-                        self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: previousPageIndex))
-                        self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: self.currentPageIndex))
-                        self.contentViews.addObject(self.imageViewCopy!)
+                        self.imageViewCopy?.sd_setImage(with: URL(string: self.imageURLArray[0]))
+                        self.contentViews.add(self.fetchContentViewAtIndex!(previousPageIndex))
+                        self.contentViews.add(self.fetchContentViewAtIndex!(self.currentPageIndex))
+                        self.contentViews.add(self.imageViewCopy!)
                     }
                 }else{
-                    self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: previousPageIndex))
-                    self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: self.currentPageIndex))
-                    self.contentViews.addObject(self.fetchContentViewAtIndex!(pageIndex: rearPageIndex))
+                    self.contentViews.add(self.fetchContentViewAtIndex!(previousPageIndex))
+                    self.contentViews.add(self.fetchContentViewAtIndex!(self.currentPageIndex))
+                    self.contentViews.add(self.fetchContentViewAtIndex!(rearPageIndex))
                 }
             }
         }
     }
     
-    func getValidNextPageIndexWithPageIndex(currentPageIndex: NSInteger) -> NSInteger {
+    
+    func getValidNextPageIndexWithPageIndex(_ currentPageIndex: NSInteger) -> NSInteger {
         if currentPageIndex == -1 {
             return _totalPageCount! - 1
         }else{
@@ -160,8 +184,8 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
     }
     
     func animationTimerDidFired() {
-        let totalCount = round(self.scrollView!.contentOffset.x) / CGRectGetWidth(UIScreen.mainScreen().bounds)
-        let newOffSet = CGPointMake((totalCount + 1) * CGRectGetWidth(UIScreen.mainScreen().bounds), self.scrollView!.contentOffset.y)
+        let totalCount = round(self.scrollView!.contentOffset.x) / UIScreen.main.bounds.width
+        let newOffSet = CGPoint(x: (totalCount + 1) * UIScreen.main.bounds.width, y: self.scrollView!.contentOffset.y)
         if self.pageControl!.currentPage + 1 > (_totalPageCount! - 1) {
             self.pageControl?.currentPage = 0
         }else{
@@ -171,21 +195,21 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
     }
     
     //MARK: UIScrollViewDelegate的协议方法
-    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
        self.animationTimer?.invalidate()
         self.animationTimer = nil
     }
     
-    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if _totalPageCount > 1 {
           self.startTimer()
         }
     }
     
     
-    func scrollViewDidScroll(scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let contentOffsetX = scrollView.contentOffset.x
-        if contentOffsetX >= (2 * CGRectGetWidth(UIScreen.mainScreen().bounds)) {
+        if contentOffsetX >= (2 * UIScreen.main.bounds.width) {
             self.currentPageIndex = self.getValidNextPageIndexWithPageIndex(self.currentPageIndex + 1)
             self.pageControl?.currentPage = self.currentPageIndex
             self.configContentViews()
@@ -197,16 +221,16 @@ class CycleScrollView: UIView, UIScrollViewDelegate {
         }
     }
 
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
 //        scrollView.setContentOffset(CGPointMake(CGRectGetWidth(UIScreen.mainScreen().bounds), 0), animated: true)
     }
 
     /**
      *  点击事件
      */
-    func contentViewTapAction(tap: UITapGestureRecognizer) {
+    func contentViewTapAction(_ tap: UITapGestureRecognizer) {
         if self.tapActionBlock != nil {
-            self.tapActionBlock!(pageIndex:self.currentPageIndex)
+            self.tapActionBlock!(self.currentPageIndex)
         }
     }
 
--- 图片轮播/ViewController.swift
+++ 图片轮播/ViewController.swift
@@ -18,22 +18,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false
-        let cycleView = CycleScrollView.init(frame: CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 200), animationDuration: 3.0)
-        cycleView.backgroundColor = UIColor.orangeColor()
+        let cycleView = CycleScrollView.init(frame: CGRect(x: 0, y: 64, width: self.view.frame.width, height: 200), animationDuration: 3.0)
+        cycleView.backgroundColor = UIColor.orange
         self.view.addSubview(cycleView)
         //设置轮播图的数组URL
         let dataArray = ["http://img04.sogoucdn.com/app/a/100520020/50c11a6a4b7a4da664e93a9cf4c061ce",
                          "http://img04.sogoucdn.com/app/a/100520024/1f9163519dac6b2138c7d96b5598467e",
                          "http://img01.sogoucdn.com/app/a/100520024/e2f057ede9d3cafabed15418bad2ee17",
-                         "http://img01.sogoucdn.com/app/a/100520020/2001859ba6fca0a525728c7568782d89",
                          "http://img04.sogoucdn.com/app/a/100520024/f4d580ab0d9f5d514c9471b23bba0561",
                          "http://img03.sogoucdn.com/app/a/100520024/30e8009fb8710f519b565b1cd17df7ec",
                          "http://img02.sogoucdn.com/app/a/100520020/992e6ea334d3d1c34abfa5ea1ec0978a"]
         //创建加载图片的ImageView
         for index in 0..<dataArray.count {
-            let imageView = UIImageView.init(frame: CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 200))
-            imageView.sd_setImageWithURL(NSURL(string: dataArray[index]))
-            self.imageViewArray.addObject(imageView)
+            let imageView = UIImageView.init(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 200))
+            imageView.sd_setImage(with: URL(string: dataArray[index]))
+            self.imageViewArray.add(imageView)
         }
         //闭包返回相应的View
         cycleView.fetchContentViewAtIndex = {(pageIndex: NSInteger) in
